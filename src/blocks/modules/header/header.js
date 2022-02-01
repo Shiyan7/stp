182211ec781046eec6a35140eef733b42200ab84
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-01 19:53:58 +0200

    fix men

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -4,51 +4,37 @@ const mainMenuLink = document.querySelector(".main-menu__link--active");
 document.addEventListener("click", (e) => {
     if(!e.target.closest(".main-menu__item")) {
         mainMenuLink?.classList.remove("active");
-        mainMenuLink?.parentElement.querySelector(".menu-dropdown")?.classList.remove("show")
+        mainMenuLink?.parentElement.querySelector(".menu-dropdown")?.classList.remove("show");
     }
 });
 
 mainMenuLink?.addEventListener("click", (e) => {
     e.currentTarget.classList.toggle("active");
-    e.currentTarget.parentElement.querySelector(".menu-dropdown")?.classList.toggle("show")
+    e.currentTarget.parentElement.querySelector(".menu-dropdown")?.classList.toggle("show");
 });
 
-for(let i = 0; i < navItem.length; i++) {
-    let el = navItem[i];
-    let flag = 0;
-    el.addEventListener("click", (e) => {
-
-        document.querySelector(".nav-dropdown.show")?.classList.remove("show")
-        document.querySelector(".nav__item.active")?.classList.remove("active")
-
-        if (flag == 0) {
-            e.currentTarget.querySelector(".nav-dropdown").classList.toggle("show");
-            e.currentTarget.classList.toggle("active");
-            flag++
-        } else {
-            e.currentTarget.querySelector(".nav-dropdown").classList.remove("show");
-            e.currentTarget.classList.remove("active");
-            flag--
+const headerMenu = () => {
+    if(window.innerWidth <= 1024) {
+        for(let i = 0; i < navItem.length; i++) {
+            let el = navItem[i];
+            el.addEventListener("click", (e) => {
+                e.currentTarget.querySelector(".nav-dropdown").classList.toggle("show");
+                e.currentTarget.classList.toggle("active");
+            });
         }
-    });
+    }
+};
 
-    document.addEventListener("click", e => {
-        if(!e.target.closest(".nav")) {
-            document.querySelectorAll(".nav-dropdown").forEach(el => el.classList.remove("show"));
-            document.querySelectorAll(".nav__item").forEach(el => el.classList.remove("active"));
-        }
-    })
-}
+headerMenu();
 
 const changeHeight = () => {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
   
-  changeHeight();
+changeHeight();
   
   
-  window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     changeHeight();
-  });
-  
+});
